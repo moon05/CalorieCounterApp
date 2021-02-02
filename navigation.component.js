73953346c7400a3369c968115108b9bea59abcb0
Abdullah Al Mamun <amamun@u.rochester.edu>
2021-02-01 19:01:51 -0500

    changed to tab.navigation for prop passing in the future

--- navigation.component.js
+++ navigation.component.js
@@ -1,35 +1,79 @@
 import React from 'react';
-import { HomeScreen } from './homeScreen.component';
-import { LogScreen } from './logScreen.component';
-import { MoreScreen } from './moreScreen.component';
-import { ProgressScreen } from './progressScreen.component';
+import {HomeScreen} from './homeScreen.component';
+import {LogScreen} from './logScreen.component';
+import {MoreScreen} from './moreScreen.component';
+import {ProgressScreen} from './progressScreen.component';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
-import { BottomNavigation } from 'react-native-paper';
 
+// import {BottomNavigation} from 'react-native-paper';
 
-const AppNavigator = () => {
-    const [index, setIndex] = React.useState(0);
-    const [routes] = React.useState([
-        { key: 'home', title: 'Home', icon: 'home' },
-        { key: 'log', title: 'Log', icon: 'book' },
-        { key: 'progress', title: 'Progress', icon: 'chart-bar' },
-        { key: 'more', title: 'More', icon: 'more' },
-    ]);
+const Tab = createMaterialBottomTabNavigator();
 
-    const renderScene = BottomNavigation.SceneMap({
-        home: HomeScreen,
-        log: LogScreen,
-        progress: ProgressScreen,
-        more: MoreScreen
-    });
+const AppNavigator = ({database}) => {
+    // const [index, setIndex] = React.useState(0);
+    // const [routes] = React.useState([
+    //     {key: 'home', title: 'Home', icon: 'home'},
+    //     {key: 'log', title: 'Log', icon: 'book'},
+    //     {key: 'progress', title: 'Progress', icon: 'chart-bar'},
+    //     {key: 'more', title: 'More', icon: 'more'},
+    // ]);
+    //
+    // const renderScene = BottomNavigation.SceneMap({
+    //     home: HomeScreen,
+    //     log: LogScreen,
+    //     progress: ProgressScreen,
+    //     more: MoreScreen
+    // });
+    //
+    // return (
+    //     <BottomNavigation
+    //         navigationState={{index, routes}}
+    //         onIndexChange={setIndex}
+    //         renderScene={renderScene}
+    //         db={database}
+    //     />
+    // );
 
     return (
-        <BottomNavigation
-            navigationState={{ index, routes }}
-            onIndexChange={setIndex}
-            renderScene={renderScene}
-        />
-    );
+        <Tab.Navigator
+            initialRouteName="Home"
+            shifting={true}
+            sceneAnimationEnabled={false}
+        >
+            <Tab.Screen
+                name="Home"
+                // component={HomeScreen}
+                options={{
+                    tabBarIcon: 'home',
+                }}
+            >
+                {(database)=> <HomeScreen {...database}/>}
+            </Tab.Screen>
+            <Tab.Screen
+                name="Log"
+                component={LogScreen}
+                options={{
+                    tabBarIcon: 'book',
+                }}
+            />
+            <Tab.Screen
+                name="Progress"
+                component={ProgressScreen}
+                options={{
+                    tabBarIcon: 'chart-bar',
+                }}
+            />
+            <Tab.Screen
+                name="More"
+                component={MoreScreen}
+                options={{
+                    tabBarIcon: 'more',
+                }}
+            />
+        </Tab.Navigator>
+
+    )
 };
 
 export default AppNavigator;
