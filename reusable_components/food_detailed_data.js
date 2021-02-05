a1f560eac49bb327a587d7503921aabd979aa009
Abdullah Al Mamun <amamun@u.rochester.edu>
2021-02-05 14:00:34 -0500

    later this willmoved into some sort of assets, this will be used for populating database at the very first start of app

---
+++ reusable_components/food_detailed_data.js
@@ -0,0 +1,31 @@
+
+const ribEye = {
+  name: 'Rib Eye',
+  calories: 199,
+  fat: 10.8,
+  sodium: 50.2,
+  carbohydrates: 0,
+  sugar: 0,
+  protein: 23.8,
+  imageSRC: 'blah',
+  typeOfFood: 'protein',
+  weight: 85
+}
+
+const rainbowTrout = {
+  name: 'Rainbow Trout',
+  calories: 103,
+  fat: 2.94,
+  sodium: 41,
+  carbohydrates: 0,
+  sugar: 0,
+  protein: 17.4,
+  imageSRC: 'blah',
+  typeOfFood: 'protein',
+  weight: 85
+}
+
+export const FOOD_OBJECTS = {
+  ribEye,
+  rainbowTrout
+}
