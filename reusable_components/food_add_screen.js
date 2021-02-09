b097db6b67b69027cd93ae08b59eb1da2258b585
Abdullah Al Mamun <amamun@u.rochester.edu>
2021-02-09 15:08:19 -0500

    added nutrition facts table to add_food_screen with styling

--- reusable_components/food_add_screen.js
+++ reusable_components/food_add_screen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { SafeAreaView, Image, View, FlatList, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { SafeAreaView, Image, View, FlatList, StyleSheet, ScrollView } from 'react-native'
 import { Divider, Text } from 'react-native-paper'
 import { DatabaseContext } from '../context/DatabaseContext'
 
@@ -56,13 +56,52 @@ export const FOOD_ADD_SCREEN = ({ propDB, route, navigation }) => {
       marginBottom: 20
     },
 
-    food_nutrition_container_4: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+    nutrition_fact_table_container: {
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
       marginTop: 40,
       marginLeft: 10,
       marginRight: 10
+    },
+
+    nutrition_fact_header_view: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 15
+    },
+
+    nutrition_fact_header_text: {
+      fontSize: 18
+    },
+
+    nutrition_fact_row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+
+    divider_nutrition_fact_row: {
+      marginTop: 20,
+      marginBottom: 20
+    },
+
+    nutrition_fact_title_view: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
+    },
+    nutrition_fact_title_text: {
+      fontSize: 14
+    },
+    nutrition_fact_value_view: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center'
+    },
+    nutrition_fact_value_text: {
+      fontSize: 14,
+      color: 'grey'
     },
 
     food_nutrition_views: {
@@ -107,55 +146,138 @@ export const FOOD_ADD_SCREEN = ({ propDB, route, navigation }) => {
 
   return (
         <SafeAreaView style={{ flex: 1 }}>
-            <View style={styles.large_container}>
-                <View style={styles.food_nutrition_container_1}>
-                    <View style={styles.food_title_view}>
-                        <Text style={styles.food_title_text}> {route.params.foodInfo.name}</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.large_container}>
+                    <View style={styles.food_nutrition_container_1}>
+                        <View style={styles.food_title_view}>
+                            <Text style={styles.food_title_text}> {route.params.foodInfo.name}</Text>
+                        </View>
+                        <View style={styles.food_weight_view}>
+                            <Text style={styles.food_weight_text}> {route.params.foodInfo.weight}g</Text>
+                        </View>
                     </View>
-                    <View style={styles.food_weight_view}>
-                        <Text style={styles.food_weight_text}> {route.params.foodInfo.weight}g</Text>
+
+                    <Divider style={styles.divider_food_nutrition_containers}/>
+
+                    <View style={styles.food_nutrition_container_2}>
+                        <View style={styles.food_nutrition_views}>
+                            <Text style={styles.food_calorie_text}> {route.params.foodInfo.calories} </Text>
+                            <Text category="h6"> Cal </Text>
+                        </View>
+                        <View style={styles.food_nutrition_views}>
+
+                            <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.carbohydrates} g </Text>
+                            <Text category="h6"> Carbs</Text>
+                        </View>
+                        <View style={styles.food_nutrition_views}>
+
+                            <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.fat} g </Text>
+                            <Text category="h6"> Fat </Text>
+                        </View>
+                        <View style={styles.food_nutrition_views}>
+
+                            <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.protein} g </Text>
+                            <Text category="h6"> Protein</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.food_nutrition_container_3}>
+                        <View>
+                            <Text>Number of Servings</Text>
+                        </View>
+                        <View>
+                            <Text style={{ color: 'blue', fontWeight: '300' }}>1</Text>
+                        </View>
+                    </View>
+
+                    <Divider style={styles.divider_food_nutrition_containers}/>
+
+                    <View style={styles.nutrition_fact_table_container}>
+
+                        <View style={styles.nutrition_fact_header_view}>
+                            <Text style={styles.nutrition_fact_header_text}> Nutrition Facts </Text>
+                        </View>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Calories </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.calories} </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Total Fat </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.fat} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Sodium </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.sodium} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Carbohydrates </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.carbohydrates} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Sugar </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.sugar} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Protein </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.protein} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
+                        <View style={styles.nutrition_fact_row}>
+                            <View style={styles.nutrition_fact_title_view}>
+                                <Text style={styles.nutrition_fact_title_text}> Protein </Text>
+                            </View>
+                            <View style={styles.nutrition_fact_value_view}>
+                                <Text style={styles.nutrition_fact_value_text}> {route.params.foodInfo.protein} g </Text>
+                            </View>
+                        </View>
+
+                        <Divider style={styles.divider_nutrition_fact_row}/>
+
                     </View>
                 </View>
-
-                <Divider style={styles.divider_food_nutrition_containers}/>
-
-                <View style={styles.food_nutrition_container_2}>
-                    <View style={styles.food_nutrition_views}>
-                        <Text style={styles.food_calorie_text}> {route.params.foodInfo.calories} </Text>
-                        <Text category="h6"> Cal </Text>
-                    </View>
-                    <View style={styles.food_nutrition_views}>
-
-                        <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.carbohydrates} g </Text>
-                        <Text category="h6"> Carbs</Text>
-                    </View>
-                    <View style={styles.food_nutrition_views}>
-
-                        <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.fat} g </Text>
-                        <Text category="h6"> Fat </Text>
-                    </View>
-                    <View style={styles.food_nutrition_views}>
-
-                        <Text style={styles.food_other_nutrition_text}> {route.params.foodInfo.protein} g </Text>
-                        <Text category="h6"> Protein</Text>
-                    </View>
-                </View>
-
-                <View style={styles.food_nutrition_container_3}>
-                    <View>
-                        <Text>Number of Servings</Text>
-                    </View>
-                    <View>
-                        <Text style={{ color: 'blue', fontWeight: '300' }}>1</Text>
-                    </View>
-                </View>
-
-                <Divider style={styles.divider_food_nutrition_containers}/>
-
-                <View style={styles.food_nutrition_container_4}>
-                    <Text>Nutrition Fact Table Will Go Here Later</Text>
-                </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
 
   )
